5ee03d8d09be6c77664bd6f9b001a15d6d330a73
vudinhquang <vudinhquangk53@gmail.com>
2020-08-02 00:31:44 +0900

    Tìm hiểu Dynamic Route

---
+++ first-project-nextjs/pages/admin/user/[id]/setting.tsx
@@ -0,0 +1,8 @@
+export default function AdminUserSetting() {
+    return (
+        <>
+            <h1>Admin -&gt; User -&gt; [id] -&gt; Setting Page</h1>
+        </>
+    )
+}
+// localhost:3000/admin/user
--- first-project-nextjs/pages/admin/user/index.tsx
+++
@@ -1,6 +0,0 @@
-export default function AdminUser() {
-    return (
-        <h1>Admin -&gt; User Page</h1>
-    )
-}
-// localhost:3000/admin/user
