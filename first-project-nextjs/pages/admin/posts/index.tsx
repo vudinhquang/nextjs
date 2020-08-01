6708e8e7061bb70c18e7501a0f849b1821927d4b
vudinhquang <vudinhquangk53@gmail.com>
2020-08-01 23:55:07 +0900

    Tìm hiểu về khái niệm Pages trong NextJs

---
+++ first-project-nextjs/pages/admin/posts/index.tsx
@@ -0,0 +1,5 @@
+export default function AdminPosts() {
+    return (
+        <h1>Admin -&gt; Posts Page</h1>
+    )
+}
---
+++ first-project-nextjs/pages/admin/user/index.tsx
@@ -0,0 +1,6 @@
+export default function AdminUser() {
+    return (
+        <h1>Admin -&gt; User Page</h1>
+    )
+}
+// localhost:3000/admin/user
