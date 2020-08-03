33d832407c14a9838d4b73064cab65694b9a4b8d
vudinhquang <vudinhquangk53@gmail.com>
2020-08-04 01:50:07 +0900

    Custom App Component và Typescript

--- first-project-nextjs/pages/_app.tsx
+++ first-project-nextjs/pages/_app.tsx
@@ -1,6 +1,42 @@
+import React from "react";
 import Head from "next/head";
+import { AppProps } from "next/app";
 import "../assets/style.scss";
 
+/*
+export default class MyApp extends React.Component<AppProps> {
+
+  render() {
+    return (
+      <div className="root-app">
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="stylesheet" href="/css/global.css" />
+        </Head>
+        <this.props.Component {...this.props.pageProps} />
+      </div>
+    )
+  }
+}
+*/
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <div className="root-app">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="/css/global.css" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  )
+}
+
+export default MyApp
+
+/*
 function MyApp({ Component, pageProps }) {
   // Component -> Dai dien cho 1 Page match url 
 
@@ -17,3 +53,4 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp
+*/
