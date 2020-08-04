6cf9abc0900e9b1d5ca08d07f91540de29e4abd3
vudinhquang <vudinhquangk53@gmail.com>
2020-08-05 02:22:50 +0900

    Tìm hiểu về useEffect trong Hooks

--- first-project-nextjs/pages/playground/hooks/lifecycle.tsx
+++ first-project-nextjs/pages/playground/hooks/lifecycle.tsx
@@ -1,9 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Button from "../../../components/Button";
 
 const LifeCycle = () => {
+    // Dong dau tien ... Tuong ung voi Constructor
+
     const [counter, setCounter] = useState(0);
     const [visible, setVisible] = useState(true);
 
+    // Similar to componentDidMount and componentDidUpdate:
+    useEffect(() => {
+        console.log("useEffect run");
+    });
+
+    console.log("Truoc khi return(render)");
     return (
         <div className="container">
             <h1>Play Ground - Life Cycle - React Hooks</h1>
@@ -12,6 +21,12 @@ const LifeCycle = () => {
                     // partial -> Một phần của state
                 }}>Counter Add</button>
             <p>{counter}</p>
+            <button onClick={() => {
+                    setVisible(false);
+                }}>Hide button</button>
+            {
+                visible && <Button />
+            }
         </div>
     )
 }
