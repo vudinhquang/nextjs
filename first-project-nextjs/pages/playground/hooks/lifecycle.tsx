7c8b7109299e406906f3d253c92c4b0d291c1f39
vudinhquang <vudinhquangk53@gmail.com>
2020-08-06 00:40:21 +0900

    Use Effect deps

--- first-project-nextjs/pages/playground/hooks/lifecycle.tsx
+++ first-project-nextjs/pages/playground/hooks/lifecycle.tsx
@@ -6,11 +6,35 @@ const LifeCycle = () => {
 
     const [counter, setCounter] = useState(0);
     const [visible, setVisible] = useState(true);
+    const [user, setUser] = useState({});
 
     // Similar to componentDidMount and componentDidUpdate:
+    // Run khi bat ke state nao thay doi
+    /*
     useEffect(() => {
         console.log("useEffect run");
+
+        return () => {
+            // componentWillUnmout <=> Component sẽ bị remove khỏi cấu trúc DOM 
+        }
     });
+    */
+    useEffect(() => {
+        console.log("Lang nghe su thay doi cua hai state counter va visible");
+    }, [counter, visible])
+
+    useEffect(() => {
+        // DidMount - Chay 1 lan duy nhat sau khi render lan dau tien
+        console.log("useEffect - empty deps");
+    }, [])
+
+    useEffect(() => {
+        console.log("Lang nghe su thay doi cua counter");
+    }, [counter]);
+
+    useEffect(() => {
+        console.log("Lang nghe su thay doi cua visible");
+    }, [visible]);
 
     console.log("Truoc khi return(render)");
     return (
