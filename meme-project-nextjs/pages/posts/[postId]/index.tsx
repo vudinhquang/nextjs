1cf60d0116225d977a6d0608859b93eb05ecb6d3
vudinhquang <vudinhquangk53@gmail.com>
2020-08-22 03:06:29 +0900

    Tối ưu SEO trong ứng dụng SSR

--- meme-project-nextjs/pages/posts/[postId]/index.tsx
+++ meme-project-nextjs/pages/posts/[postId]/index.tsx
@@ -1,4 +1,5 @@
 import { GetServerSideProps, NextPageContext, InferGetServerSidePropsType } from "next";
+import { NextSeo } from 'next-seo';
 
 import { HomeSidebar } from "../../../components/HomeSidebar";
 import { PostDetailContent } from "../../../components/PostDetailContent";
@@ -36,6 +37,31 @@ type PostDetailProps = React.FC<InferGetServerSidePropsType<typeof getServerSide
 const PostDetail: PostDetailProps = ({ userPosts, postDetailData, postCategories, comments }) => {
     return (
         <div className="container">
+            <NextSeo
+                title="Bai viet chi tiet"
+                description={postDetailData.post_content}
+                canonical="https://www.canonical.ie/"
+                openGraph={{
+                    title: 'Open Graph Title',
+                    description: 'Open Graph Description',
+                    images: [
+                        {
+                            url: 'https://www.example.ie/og-image-01.jpg',
+                            width: 800,
+                            height: 600,
+                            alt: 'Og Image Alt',
+                        },
+                        {
+                            url: 'https://www.example.ie/og-image-02.jpg',
+                            width: 900,
+                            height: 800,
+                            alt: 'Og Image Alt Second',
+                        },
+                        { url: 'https://www.example.ie/og-image-03.jpg' },
+                        { url: 'https://www.example.ie/og-image-04.jpg' },
+                    ]
+                }}
+            />
             {/*sections*/}
             <div className="row">
                 <div className="col-lg-8">
